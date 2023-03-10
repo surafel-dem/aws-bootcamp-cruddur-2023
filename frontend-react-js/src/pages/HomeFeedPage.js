676412a89e471beb05a0ff13716a838b485b755a
Surafel <surafel.demissie@outlook.com>
2023-03-10 13:49:10 +0000

    Cognito setup

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -6,7 +6,7 @@ import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
-
+import { Auth } from 'aws-amplify';
 // [TODO] Authenication
 import Cookies from 'js-cookie'
 
@@ -17,7 +17,7 @@ export default function HomeFeedPage() {
   const [replyActivity, setReplyActivity] = React.useState({});
   const [user, setUser] = React.useState(null);
   const dataFetchedRef = React.useRef(false);
-
+  
   const loadData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
@@ -35,15 +35,24 @@ export default function HomeFeedPage() {
     }
   };
 
+    // check if we are authenicated
   const checkAuth = async () => {
-    console.log('checkAuth')
-    // [TODO] Authenication
-    if (Cookies.get('user.logged_in')) {
+    Auth.currentAuthenticatedUser({
+    // Optional, By default is false. 
+    // If set to true, this call will send a 
+    // request to Cognito to get the latest user data
+    bypassCache: false 
+    })
+    .then((user) => {
+    console.log('user',user);
+    return Auth.currentAuthenticatedUser()
+    }).then((cognito_user) => {
       setUser({
-        display_name: Cookies.get('user.name'),
-        handle: Cookies.get('user.username')
+        display_name: cognito_user.attributes.name,
+        handle: cognito_user.attributes.preferred_username
       })
-    }
+    })
+    .catch((err) => console.log(err));
   };
 
   React.useEffect(()=>{
