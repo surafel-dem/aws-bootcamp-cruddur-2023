a901e4b95c66de01116f24d87e6e75bcf36a40af
Surafel <surafel.demissie@outlook.com>
2023-03-25 16:55:51 +0000

    Query working

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -22,11 +22,11 @@ class HomeActivities:
       LEFT JOIN public.users ON users.uuid = activities.user_uuid
       ORDER BY activities.created_at DESC
       """)
-     # print(sql)
-     # with pool.connection() as conn:
-     #   with conn.cursor() as cur:
-     #     cur.execute(sql)
+      print(sql)
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql)
           # this will return a tuple
           # the first field being the data
-    #      json = cur.fetchone()
-    #  return json[0]
+          json = cur.fetchone()
+      return json[0]
