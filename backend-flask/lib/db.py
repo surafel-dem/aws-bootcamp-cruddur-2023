0f463b7aa013c10dd178f0aef1e49cd0cdf3450a
Surafel <surafel.demissie@outlook.com>
2023-04-08 10:50:15 +0000

    Fix issues

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -8,12 +8,9 @@ class Db:
   def __init__(self):
     self.init_pool()
 
-  def template(self,*args):
-    pathing = list((app.root_path,'db','sql',) + args)
-    pathing[-1] = pathing[-1] + ".sql"
-
-    template_path = os.path.join(*pathing)
-
+  def template(self,name):
+    template_path = os.path.join(app.root_path,'db','sql',name+'.sql')
+   
     green = '\033[92m'
     no_color = '\033[0m'
     print("\n")
@@ -28,20 +25,9 @@ class Db:
     self.pool = ConnectionPool(connection_url)
   # we want to commit data such as an insert
   # be sure to check for RETURNING in all uppercases
-  def print_params(self,params):
-    blue = '\033[94m'
-    no_color = '\033[0m'
-    print(f'{blue} SQL Params:{no_color}')
-    for key, value in params.items():
-      print(key, ":", value)
-
-  def print_sql(self,title,sql,params={}):
-    cyan = '\033[96m'
-    no_color = '\033[0m'
-    print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-    print(sql,params)
-  def query_commit(self,sql,params={}):
-    self.print_sql('commit with returning',sql,params)
+  def query_commit(self,sql,params):
+    print('SQL STATEMENT-[commit with returning]----')
+    print(sql + "\n")
 
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
@@ -67,28 +53,21 @@ class Db:
         cur.execute(wrapped_sql,params)
         json = cur.fetchone()
         return json[0]
+ 
+ 
   # When we want to return an array of json objects
-  def query_object_json(self,sql,params={}):
+  def query_object_json(self,sql):
+     print('SQL STATEMENT-[object]----')
+     print(sql + "\n")
 
-    self.print_sql('json',sql,params)
-    self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
 
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
         cur.execute(wrapped_sql,params)
         json = cur.fetchone()
-        if json == None:
-          "{}"
-        else:
           return json[0]
-  def query_value(self,sql,params={}):
-    self.print_sql('value',sql,params)
-    with self.pool.connection() as conn:
-      with conn.cursor() as cur:
-        cur.execute(sql,params)
-        json = cur.fetchone()
-        return json[0]
+
   def query_wrap_object(self,template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -50,15 +50,11 @@ class CreateActivity:
     return model
 
   def create_activity(handle, message, expires_at):
-    sql = db.template('activities','create')
+    sql = db.template('create_activity')
     uuid = db.query_commit(sql,{
       'handle': handle,
       'message': message,
       'expires_at': expires_at
     })
     return uuid
-  def query_object_activity(uuid):
-    sql = db.template('activities','object')
-    return db.query_object_json(sql,{
-      'uuid': uuid
-    })
+
